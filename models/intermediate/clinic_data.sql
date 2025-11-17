aa3c85b4e9e8e6f0e84e2f8d657cbb4a6c99eaf6
NinadMohiteUmmeed <ninad.mohite@ummeed.org>
2025-11-17 16:45:44 +0530

    Coalesced doctor level in CBD_And_Calculate_Age CTE

--- models/intermediate/clinic_data.sql
+++ models/intermediate/clinic_data.sql
@@ -145,7 +145,8 @@ Base_Clinic_Data AS (
         ctm."New Classification" AS consultation_category,  -- Mapped from dim_consultation_type_mapping
         CONCAT_WS(' ', dda.acronym, ctm."New Classification") AS dep_consult_category,  -- Acronym + Consultation Category
         dda.acronym AS dep_shortened,
-        COALESCE(p.doctor_level, 'L0') AS doctor_level  -- Mapped from promotion CTE 
+        doctor_level  -- Mapped from promotion CTE 
+        --COALESCE(p.doctor_level, 'L0') AS doctor_level  -- Mapped from promotion CTE 
     FROM clinic_data AS cd
     LEFT JOIN registered_patient AS rp
         ON cd.mrno = rp.mrno
@@ -165,6 +166,7 @@ Base_Clinic_Data AS (
 CBD_And_Calculated_Age AS (
 SELECT 
     *,
+    COALESCE(p.doctor_level, 'L0') AS doctor_lvl
     CASE
         WHEN bcd.date_of_birth IS NULL OR bcd.fiscal_year_start_date IS NULL THEN NULL
         WHEN TO_DATE(bcd.date_of_birth, 'DD/MM/YYYY')::DATE > TO_DATE(bcd.fiscal_year_start_date, 'DD/MM/YYYY')::DATE THEN CAST(0.00 AS NUMERIC)
