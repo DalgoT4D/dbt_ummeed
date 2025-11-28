d33510e21564175e46fa778e0228f00ebd21a2d3
NinadMohiteUmmeed <ninad.mohite@ummeed.org>
2025-11-28 11:26:05 +0530

    Fixed present age calculation

--- models/intermediate/clinic_data.sql
+++ models/intermediate/clinic_data.sql
@@ -179,8 +179,20 @@ SELECT
             2
         )::NUMERIC
     END AS calculated_age,
-    ROUND((TO_DATE(bcd.consultation_date, 'DD/MM/YYYY')::DATE - TO_DATE(bcd.date_of_birth, 'DD/MM/YYYY')::DATE)::NUMERIC/ CAST(365.25 AS NUMERIC)), 2)::NUMERIC AS actual_age,
-    ROUND(current_date - TO_DATE(bcd.date_of_birth, 'DD/MM/YYYY')::DATE)::NUMERIC/ CAST(365.25 AS NUMERIC)), 2)::NUMERIC AS present_age,
+        ROUND(
+        (
+            (TO_DATE(bcd.consultation_date, 'DD/MM/YYYY')::DATE - TO_DATE(bcd.date_of_birth, 'DD/MM/YYYY')::DATE)::NUMERIC
+            / CAST(365.25 AS NUMERIC)
+        ),
+        2
+    )::NUMERIC AS actual_age,
+    ROUND(
+        (
+            (current_date - TO_DATE(bcd.date_of_birth, 'DD/MM/YYYY')::DATE)::NUMERIC
+            / CAST(365.25 AS NUMERIC)
+        ),
+        2
+    )::NUMERIC AS present_age
 FROM Base_Clinic_Data AS bcd
 )
 
